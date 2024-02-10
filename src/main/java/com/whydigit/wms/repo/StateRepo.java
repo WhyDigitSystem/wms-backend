fd01fa8540b2e92f7173119962d6855f8cb9e566
Guhanwds <guhan@whydigit.com>
2024-02-10 10:52:57 +0530

    Region api done

--- src/main/java/com/whydigit/wms/repo/StateRepo.java
+++ src/main/java/com/whydigit/wms/repo/StateRepo.java
@@ -1,9 +1,6 @@
 package com.whydigit.wms.repo;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.whydigit.wms.entity.StateVO;
--- src/main/java/com/whydigit/wms/service/CommonMasterService.java
+++ src/main/java/com/whydigit/wms/service/CommonMasterService.java
@@ -7,29 +7,27 @@ import org.springframework.stereotype.Service;
 
 import com.whydigit.wms.entity.CityVO;
 import com.whydigit.wms.entity.CountryVO;
+import com.whydigit.wms.entity.RegionVO;
 import com.whydigit.wms.entity.StateVO;
 
 @Service
 public interface CommonMasterService {
-    
-	
+
 	// Country
-	
-    List<CountryVO> getAllCountry(); // Method names should be in camelCase
-    
-    Optional<CountryVO> getCountryById(Long countryid);
 
-    CountryVO createCountry(CountryVO countryVO); // Return the created entity
+	List<CountryVO> getAllCountry(); // Method names should be in camelCase
 
-    Optional<CountryVO> updateCountry(CountryVO countryVO); // The parameter name should be camelCase
+	Optional<CountryVO> getCountryById(Long countryid);
 
-    void deleteCountry(Long countryid);
-    
-    
-    // State
-    
-    List<StateVO> getAllgetAllStates();
-	
+	CountryVO createCountry(CountryVO countryVO); // Return the created entity
+
+	Optional<CountryVO> updateCountry(CountryVO countryVO); // The parameter name should be camelCase
+
+	void deleteCountry(Long countryid);
+
+	// State
+
+	List<StateVO> getAllgetAllStates();
 
 	Optional<StateVO> getStateById(Long stateid);
 
@@ -38,19 +36,28 @@ public interface CommonMasterService {
 	Optional<StateVO> updateState(StateVO stateVO);
 
 	void deleteState(Long stateid);
-	
-	
+
 	// city
-	
+
 	List<CityVO> getAllgetAllCities();
-	
+
 	Optional<CityVO> getCityById(Long cityid);
 
 	CityVO createCity(CityVO cityVO);
 
 	Optional<CityVO> updateCity(CityVO cityVO);
 
-	void deleteCity(Long id);
+	void deleteCity(Long cityid);
 
-	
+	// region
+
+	List<RegionVO> getAllRegion();
+
+	Optional<RegionVO> getRegionById(Long regionid);
+
+	RegionVO createRegion(RegionVO regionVO);
+
+	Optional<RegionVO> updateRegion(RegionVO regionVO);
+
+	void deleteRegion(Long regionid);
 }
