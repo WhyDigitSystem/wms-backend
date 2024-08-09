43eee8698557128ae68ee02422252c41fc71c37f
vasanth <vasanth@whydigit.com>
2024-08-09 16:31:19 +0530

    commit

---
+++ src/main/java/com/whydigit/wms/repo/VasPickDetailsRepo.java
@@ -0,0 +1,6 @@
+package com.whydigit.wms.repo;
+
+public interface VasPickDetailsRepo {
+
+	
+}
---
+++ src/main/java/com/whydigit/wms/repo/VasPickRepo.java
@@ -0,0 +1,14 @@
+package com.whydigit.wms.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.whydigit.wms.entity.VasPickVO;
+
+public interface VasPickRepo extends JpaRepository<VasPickVO, Long>{
+
+	
+	
+	
+	
+	
+}
