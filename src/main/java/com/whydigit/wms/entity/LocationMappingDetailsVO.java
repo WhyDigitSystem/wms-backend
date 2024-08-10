486daba1893fffc5249dbfd3af77196e5cd31431
vasanth <vasanth@whydigit.com>
2024-08-10 15:09:17 +0530

    error fix

--- src/main/java/com/whydigit/wms/entity/LocationMappingDetailsVO.java
+++ src/main/java/com/whydigit/wms/entity/LocationMappingDetailsVO.java
@@ -22,39 +22,27 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LocationMappingDetailsVO {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "locationmappingdetailsgen")
 	@SequenceGenerator(name = "locationmappingdetailsgen", sequenceName = "locationmappingdetailsseq", initialValue = 1000000001, allocationSize = 1)
 	private Long id;
-	@Column(name="branch")
-	private String branch;
-	@Column(name="branchcode")
-	private String branchCode;
-	@Column(name="warehouse")
+	@Column(name = "warehouse")
 	private String warehouse;
-	@Column(name="bintype")
-	private String binType;
-	@Column(name="clienttype")
-	private String clientType;
-	@Column(name="rowno")
+	@Column(name = "rowno")
 	private String rowNo;
-	@Column(name="levelno")
+	@Column(name = "levelno")
 	private String levelNo;
-	@Column(name="client")
-	private String client;
-	private boolean cancel;
-	@Column(name="bin")
-	private String bin;
-	@Column(name="lstatus")
-	private String lstatus;
-	@Column(name="cellcategory")
-	private String cellCategory;
-	@Column(name="core")
-	private String core;
+	@Column(name = "palletNo")
+	private String palletNo;
+	@Column(name = "binstatus")
+	private String binStatus;
+	@Column(name = "binSeq")
+	private String binSeq;
+	@Column(name = "core")
+	private String multiCore;
 	private boolean active;
-	
-	
+
 	@JsonBackReference
 	@ManyToOne
 	@JoinColumn(name = "locationmappingid")
