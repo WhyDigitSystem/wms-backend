bb6f28e2eca5ed4829ccaa84a8de9e45024ae7cc
Aravinth <justinaravinth@whydigit.com>
2024-08-16 11:19:16 +0530

    stockDetails Correction

--- src/main/java/com/whydigit/wms/repo/StockDetailsRepo.java
+++ src/main/java/com/whydigit/wms/repo/StockDetailsRepo.java
@@ -11,7 +11,7 @@ import com.whydigit.wms.entity.StockDetailsVO;
 @Repository
 public interface StockDetailsRepo extends JpaRepository<StockDetailsVO, Long> {
 
-@Query(nativeQuery =true,value ="select sum(sqty) from stockdetails  WHERE orgid = ?1 AND client = ?3 AND branchcode = ?2 AND warehouse = ?5 AND partno =?4 and grnno=?6 and bin='BULK' AND status = 'V';")
+@Query(nativeQuery =true,value ="select sum(sqty) from stockdetails  WHERE orgid = ?1 and pckey='CHILD' AND client = ?3 AND branchcode = ?2 AND warehouse = ?5 AND partno =?4 and grnno=?6 and bin='BULK' AND status = 'V';")
 	Set<Object[]> getQtyDetais(Long orgId, String branchCode, String client, String partNo, String warehouse,String grnno);
 
 }
