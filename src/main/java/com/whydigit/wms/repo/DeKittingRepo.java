f250305d39db1ea3672d9ea0caaa395e161960a7
Guhanwhydigit <guhan@whydigit.in>
2024-08-24 11:50:40 +0530

    PARENT added in dekitting

--- src/main/java/com/whydigit/wms/repo/DeKittingRepo.java
+++ src/main/java/com/whydigit/wms/repo/DeKittingRepo.java
@@ -20,13 +20,13 @@ public interface DeKittingRepo extends JpaRepository<DeKittingVO, Long> {
 	@Query(nativeQuery = true, value = "select concat(prefixfield,lpad(lastno,6,'0')) AS docid from m_documenttypemappingdetails where orgid=?1 and finyear=?2 and branchcode=?3 and client =?4 and screencode=?5")
 	String getDeKittingDocId(Long orgId, String finYear, String branchCode, String client, String screenCode);
 
-	@Query(nativeQuery = true, value = "select partno,partdesc,sku,sum(sqty) as sqty from stockdetails where orgid=?1 and branch=?2 AND branchcode=?3 and client=?4 GROUP BY partno,partdesc,sku HAVING sum(sqty)>0")
+	@Query(nativeQuery = true, value = "select partno,partdesc,sku,sum(sqty) as sqty from stockdetails where orgid=?1 and branch=?2 AND branchcode=?3 and client=?4  and parentchildkey='PARENT' GROUP BY partno,partdesc,sku HAVING sum(sqty)>0")
 	Set<Object[]> findPartNoFromStockForDeKittingParent(Long orgId, String branch, String branchCode, String client);
 
-	@Query(nativeQuery = true, value = "select bin,binclass,bintype,sum(sqty) as avlqty from stockdetails  where orgid=?1 and branch=?2 and branchcode=?3 and client=?4 GROUP BY bin,binclass,bintype HAVING SUM(SQTY) > 0")
+	@Query(nativeQuery = true, value = "select bin,binclass,bintype,sum(sqty) as avlqty from stockdetails  where orgid=?1 and branch=?2 and branchcode=?3 and client=?4 and parentchildkey='PARENT' GROUP BY bin,binclass,bintype HAVING SUM(SQTY) > 0")
 	Set<Object[]> findBinFromStockForDeKittingParent(Long orgId, String branch, String branchCode, String client);
 
-	@Query(nativeQuery = true, value = "select grnno,batch,batchdate,expdate,sum(sqty) as sqty from stockdetails  where orgid=?1 and branch=?2 and branchcode=?3 and client=?4 and bin=?5 and partno=?6 and partdesc=?7 and sku=?8 GROUP BY grnno,batch,batchdate,expdate HAVING sum(sqty)>0")
+	@Query(nativeQuery = true, value = "select grnno,batch,batchdate,expdate,sum(sqty) as sqty from stockdetails  where orgid=?1 and branch=?2 and branchcode=?3 and client=?4 and bin=?5 and partno=?6 and partdesc=?7 and sku=?8 and parentchildkey='PARENT' GROUP BY grnno,batch,batchdate,expdate HAVING sum(sqty)>0")
 	Set<Object[]> findGrnNoAndBatchAndBatchDateAndExpDateFromStockForDeKittingParent(Long orgId, String branch,
 			String branchCode, String client, String bin, String partNo, String partDesc, String sku);
 
@@ -34,7 +34,7 @@ public interface DeKittingRepo extends JpaRepository<DeKittingVO, Long> {
 	Set<Object[]> findPartNoAndPartDescAndSkuFromMaterialForDeKittingChild(Long orgId, String branch, String branchCode,
 			String client);
 
-	@Query(nativeQuery = true,value="select sum(a.avlqty)fromqty from(select partno,partdesc,grnno,branch,branchcode,client,bin,sku,sum(sqty)as avlqty from stockdetails where  orgid=?1 and branch=?2 AND branchcode=?3 and client=?4 and bin =?5 and partdesc =?6 and sku=?7  and partno=?8 and grnno=?9 group by partno,partdesc,grnno,branch,branchcode,client,bin,sku having sum(sqty)>0) a")
+	@Query(nativeQuery = true,value="select sum(a.avlqty)fromqty from(select partno,partdesc,grnno,branch,branchcode,client,bin,sku,sum(sqty)as avlqty from stockdetails where  orgid=?1 and branch=?2 AND branchcode=?3 and client=?4 and bin =?5 and partdesc =?6 and sku=?7  and partno=?8 and grnno=?9 and parentchildkey='PARENT' group by partno,partdesc,grnno,branch,branchcode,client,bin,sku having sum(sqty)>0) a")
 	Set<Object[]> findAvlQtyFromStockForDeKittingParent(Long orgId, String branch, String branchCode, String client,
 			String bin, String partDesc, String sku, String partNo, String grnNo);
 }
